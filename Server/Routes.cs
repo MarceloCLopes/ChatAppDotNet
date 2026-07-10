4b09a012bc356e95df2c9966487397a698d0b805
MarceloCLopes <marc01mcl@hotmail.com>
2026-07-10 14:42:08 -0300

    Server project configuration

---
+++ Server/Routes.cs
@@ -0,0 +1,13 @@
+﻿namespace Server;
+
+public static class Routes
+{
+    public static void MapRoutes(this WebApplication app)
+    {
+        var route = app.MapGroup("");
+
+        route.MapGet("", () => "Server is up and running");
+        // Add more routes here
+    }
+
+}
