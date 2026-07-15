28f98191dd381b9cf982b5e2a978f3f7daf9462c
MarceloCLopes <marc01mcl@hotmail.com>
2026-07-15 14:09:51 -0300

    Creating and configuring chat rooms

---
+++ Server/ChatHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Server;
+
+public class ChatHub : Hub
+{
+    public override Task OnConnectedAsync()
+    {
+        Console.WriteLine("Temos uma nova conexão!!!");
+        return base.OnConnectedAsync();
+    }
+
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        Console.WriteLine("O usuário saiu!!!");
+        return base.OnDisconnectedAsync(exception);
+    }
+
+    public async Task JoinRoom(string name, string room)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, room);
+        await Clients.Group(room).SendAsync("ReceiveMessage", $"{name} entrou na sala {room}");
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -3,6 +3,19 @@ using Server;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowBlazorClient",
+        policy =>
+        {
+            policy.WithOrigins("https://localhost:7000", "http://localhost:5125") // URL do Blazor WebAssembly
+                  .AllowAnyHeader()
+                  .AllowAnyMethod()
+                  .AllowCredentials(); // necessário para SignalR
+        });
+});
+
 builder.Services.AddSignalR();
 
 var app = builder.Build();
@@ -12,6 +25,12 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.UseRouting();
+// Ativa CORS antes de mapear endpoints
+app.UseCors("AllowBlazorClient");
+
 app.MapRoutes();
 app.UseHttpsRedirection();
+
+app.MapHub<ChatHub>("/chatHub");
 app.Run();
